aba1f5e3fed18b5a9d8f761649eb1a2a5aae4c2b
Hog <hogman12333@gmail.com>
2026-03-16 19:21:52 +0000

    Update start_gui.py

--- src/lufus/gui/start_gui.py
+++ src/lufus/gui/start_gui.py
@@ -1,13 +1,17 @@
 import sys
 import os
+from lufus.lufus_logging import get_logger, setup_logging
 from lufus.drives.find_usb import find_usb
+
+setup_logging()
+log = get_logger(__name__)
 
 
 def ensure_root():
     # this function checks for x11 or wayland and asks for root perms
     # it also fixes any display issues that might happen due to wrong perm management
     if os.geteuid() != 0:
-        print("Need admin rights. Spawning pkexec...")
+        log.info("Need admin rights. Spawning pkexec...")
         gui_env = {
             "DISPLAY": os.environ.get("DISPLAY"),
             "XAUTHORITY": os.environ.get("XAUTHORITY")
@@ -24,13 +28,17 @@ def ensure_root():
         appimage = os.environ.get("APPIMAGE")
         executable = appimage if appimage else sys.executable
         cmd = ["pkexec"] + env_args + [executable] + (sys.argv[1:] if appimage else sys.argv)
+        log.debug("pkexec command: %s", cmd)
         os.execvp("pkexec", cmd)
+
 usb_devices = find_usb()
+
 
 def launch_gui_with_usb_data() -> None:
     ensure_root()
     #usb_devices = find_usb()
-    #print("Detected USB devices:", usb_devices)
+
+    log.info("Launching GUI with USB devices: %s", usb_devices)
 
     from PyQt6.QtWidgets import QApplication
     from lufus.gui.gui import lufus as LufusWindow
